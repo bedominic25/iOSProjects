9575f1be3d812afffc98005cee684a007ff37106
benji4720148 <bmutebi2@gmail.com>
2016-09-12 17:26:08 -0400

    changes

---
+++ iOSTest/IOSProgrammerTest/Table Section/ChatCell.m
@@ -0,0 +1,32 @@
+//
+//  TableSectionTableViewCell.m
+//  IOSProgrammerTest
+//
+//  Created by Justin LeClair on 12/15/14.
+//  Copyright (c) 2014 AppPartner. All rights reserved.
+//
+
+#import "ChatCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+@interface ChatCell ()
+
+@end
+
+@implementation ChatCell
+
+- (void)awakeFromNib
+{
+    
+}
+
+- (void)loadWithData:(ChatData *)chatData
+{
+    self.usernameLabel.text = chatData.username;
+    self.messageTextView.text = chatData.message;
+    [self.imageView sd_setImageWithURL:[NSURL URLWithString:chatData.avatar_url]placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    
+}
+
+
+@end
