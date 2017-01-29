771ce38b11992ef23a5f54546bffd1c54a0a5b78
benji4720148 <bmutebi2@gmail.com>
2017-01-29 15:43:32 -0500

    collectionView

---
+++ PubNub/CollectionViewDemo/CollectionViewDemo/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by 1800 on 1/18/17.
+//  Copyright © 2017 1800. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
